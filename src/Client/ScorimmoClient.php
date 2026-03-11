aaa3ee9c1a08e0a3a77b09777491285bdcbc04ad
Luc <luc@scorimmo.com>
2026-03-11 13:33:03 +0100

    fix: remove deprecated curl_close() call (PHP 8.5)
    curl_close() is deprecated since PHP 8.5 as it has had no effect since PHP 8.0.

--- src/Client/ScorimmoClient.php
+++ src/Client/ScorimmoClient.php
@@ -86,7 +86,6 @@ class ScorimmoClient
 
         $raw = curl_exec($ch);
         $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
 
         if ($raw === false) {
             throw new ScorimmoApiException('cURL request failed', 0);
